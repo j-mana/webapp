508d7ff13cfa5433f2baff58496f62d072aaf4cc
John Paul <johnarpaul@gmail.com>
2025-07-10 12:17:08 +0100

    added chat quick recommendations

--- app/(platform)/page.tsx
+++ app/(platform)/page.tsx
@@ -4,11 +4,11 @@ import { useState } from 'react'
 import ChatIcon from "@/icons/ChatIcon"
 import TabGroup from "@/components/TabGroup"
 import StyleIcon from "@/icons/StyleIcon"
-import SendIcon from '@/icons/SendIcon'
 import SelectionIcon from '@/icons/SelectionIcon'
 import GridIcon from '@/icons/GridIcon'
 import Header from '@/components/Header'
 import Canvas from '@/components/Canvas'
+import ChatInput from '@/components/ChatInput'
 
 export default function App() {
   const [activeMainTab, setActiveMainTab] = useState('chat')
@@ -35,16 +35,7 @@ export default function App() {
               defaultTab="chat"
               onTabChange={setActiveMainTab}
             />
-            <div className='flex flex-col gap-2 bg-white rounded-2xl border border-border-light mt-auto strong-shadow'>
-              <textarea className='w-full h-full p-4 resize-none outline-none text-sm' placeholder='Create or edit...'></textarea>
-
-              <div className='flex flex-row p-2'>
-                <div className='px-3 py-1.5 bg-black text-white flex flex-row gap-1 items-center rounded-xl w-max ml-auto'>
-                  <p className='text-base text-white font-medium'>Send</p>
-                  <SendIcon size={14}/>
-                </div>
-              </div>
-            </div>
+            <ChatInput />
           </div>
           <div className='col-span-3 overflow-auto h-full w-full border border-border-light rounded-xl divide-y divide-border-light'>
             <div className='flex flex-row gap-2 items-center w-full bg-canvas px-4 py-2 sticky top-0 z-10'>
---
+++ components/ChatInput.tsx
@@ -0,0 +1,49 @@
+import LayoutAltIcon from "@/icons/LayoutAltIcon";
+import PaintPourIcon from "@/icons/PaintPourIcon";
+import SendIcon from "@/icons/SendIcon";
+import TextInputIcon from "@/icons/TextInputIcon";
+import { useEffect, useState } from "react";
+
+export default function ChatInput() {
+
+  const [activeRecommendation, setActiveRecommendation] = useState<string | null>(null)
+  const [input, setInput] = useState<string>('')
+
+  useEffect(() => {
+    if (activeRecommendation) {
+      setInput(`${activeRecommendation}...`)
+    } else {
+      setInput('')
+    }
+  }, [activeRecommendation])
+  
+  return (
+    <div className='flex flex-col gap-2 mt-auto'>
+
+      <div className='flex flex-wrap gap-2 mx-auto items-center justify-center'>
+        <ChatRecommendation label="Personalize hero" icon={<TextInputIcon size={12}/>} active={activeRecommendation === 'personalize'} onClick={() => setActiveRecommendation('personalize')}/>
+        <ChatRecommendation label="Improve layout" icon={<LayoutAltIcon size={12}/>} active={activeRecommendation === 'layout'} onClick={() => setActiveRecommendation('layout')}/>
+        <ChatRecommendation label="Color CTAs" icon={<PaintPourIcon size={12}/>} active={activeRecommendation === 'ctas'} onClick={() => setActiveRecommendation('ctas')}/>
+      </div>
+
+      <div className='flex flex-col gap-2 bg-white rounded-2xl border border-border-light strong-shadow'>
+        <textarea className='w-full h-full p-4 resize-none outline-none text-sm' placeholder='Create or edit...' value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+        <div className='flex flex-row p-2'>
+          <div className='px-3 py-1.5 bg-black text-white flex flex-row gap-1 items-center rounded-xl w-max ml-auto'>
+            <p className='text-base text-white font-medium'>Send</p>
+            <SendIcon size={14}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const ChatRecommendation = ({label, icon, active, onClick}: {label: string, icon: React.ReactNode, active: boolean, onClick: () => void}) => {
+  return (
+    <div className={`shrink-0 flex flex-row gap-1 items-center border rounded-lg p-1.5 py-1 transition-colors duration-200 ${active ? 'bg-black text-white border-transparent' : 'bg-canvas text-gray-600 border-border-light hover:bg-gray-50 cursor-pointer'}`} onClick={onClick}>
+      {icon}
+      <p className='text-tiny shrink-0'>{label}</p>
+    </div>
+  )
+}
