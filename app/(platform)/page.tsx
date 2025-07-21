cc0126b7ca5ff4d3197005f32adae36d7b3ff1a8
John Paul <johnarpaul@gmail.com>
2025-07-21 16:42:25 +0100

    Update page.tsx

--- app/(platform)/page.tsx
+++ app/(platform)/page.tsx
@@ -27,30 +27,8 @@ export default function App() {
   return (
     <div className='h-dvh w-full flex flex-col'>
       <Header />
-      <div className='flex flex-1 overflow-hidden'>
-        <div className='flex flex-row h-full w-full'>
-          <div className='p-4 flex flex-col w-96 min-w-96'>
-            <TabGroup 
-              tabs={mainTabs}
-              defaultTab="chat"
-              onTabChange={setActiveMainTab}
-            />
-            <ChatInput input={''} setInput={() => {}} onSend={() => {}} />
-          </div>
-          <div className='overflow-auto h-full w-full border border-border-light rounded-tl-xl divide-y divide-border-light'>
-            <div className='flex flex-row gap-2 items-center w-full bg-canvas px-4 py-2 sticky top-0 z-10'>
-              <TabGroup 
-                tabs={secondaryTabs}
-                defaultTab="edit"
-                onTabChange={setActiveSecondaryTab}
-              />
-            </div>
-            <div className='h-full w-full bg-gray-50'>
-              <Canvas />
-            </div>
-          </div>
-        </div>
-      </div>
+      
+      Homepage
     </div>
   )
 }
